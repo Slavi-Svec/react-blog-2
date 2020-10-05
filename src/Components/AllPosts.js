e0a7968e7b90469a128994ebfe9a575cc7214651
izaak svec <slavi.svec@gmail.com>
2020-10-05 14:13:40 +1100

    Complete all blog posts styles

--- src/Components/AllPosts.js
+++ src/Components/AllPosts.js
@@ -25,17 +25,23 @@ export default function AllPosts() {
 
   return (
     <div className="bg-green-100 min-h-screen p-12">
-      <div>
-        <h2>Blog Posts 2020</h2>  
-        <h3>Welcome to my blog</h3>
-        <div>
+      <div className="container mx-auto">
+        <h2 className="text-5xl flex justify-center">Blog Posts 2020</h2>
+        <h3 class="text-lg text-gray-500 flex justify-center mb-12">
+          Welcome to my blog
+        </h3>
+        <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
           {allPostsData &&
             allPostsData.map((post, index) => (
               <Link to={"/" + post.slug.current} key={post.slug.current}>
-                <span key={index}>
-                  <img src={post.mainImage.asset.url} alt="" />
-                  <span>
-                    <h2>{post.title}</h2>
+                <span className="block bg-white block h-64 relative rounded shadow leading-snug border-l-8 border-green-800" key={index}>
+                  <img
+                    className="w-full h-full rounded-r object-cover absolute"
+                    src={post.mainImage.asset.url}
+                    alt=""
+                  />
+                  <span className="block relative h-full flex justify-end items-end pr-4 pb-4">
+                    <h2 className="text-gray-800 text-lg font-bold px-3 py-4 bg-red-700 text-red-100 bg-opacity-75 rounded">{post.title}</h2>
                   </span>
                 </span>
               </Link>
