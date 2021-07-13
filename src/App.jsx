bf9f9988d94117de7c737efa61ff75ecea955e3b
izaak svec <slavi.svec@gmail.com>
2021-07-13 15:10:25 +1000

    Add ScrollToTop component

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
 import React from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import Navbar from './Components/Navigation/Navigation.jsx'
+import ScrollToTop from './Components/ScrollToTop/ScollToTop.jsx'
 import AllPosts from './Components/AllPosts/AllPosts.jsx'
 import OnePost from './Components/OnePost/OnePost.jsx'
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Navbar />
-      <div>
-        <Route component={AllPosts} path="/" exact />
-        <Route component={OnePost} path="/:slug" />
-      </div>
+      <ScrollToTop>
+        <Navbar />
+        <div>
+          <Route component={AllPosts} path="/" exact />
+          <Route component={OnePost} path="/:slug" />
+        </div>
+      </ScrollToTop>
     </BrowserRouter>
   )
 }
